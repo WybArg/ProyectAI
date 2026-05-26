356c8c8528000a506de60fcd127f3ea3e8ff7913
Walter <cbzwyb@gmail.com>
2026-05-25 22:09:27 -0300

    agrege archivo Hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+# ==============================================================================
+# SCRIPT: hola_mundo.py
+# DESCRIPCIÓN: Este script imprime el mensaje "Hola Mundo" en la consola
+#              siguiendo las reglas de estilo del proyecto.
+# ==============================================================================
+
+# Imprimir el mensaje de saludo en pantalla
+print("Hola Mundo") # Muestra el texto "Hola Mundo" por la terminal.
